b31d26ea1bfe8e102f706928e8016a4e6053393e
Adam Thomas <thomasadam83@hotmail.com>
2022-12-30 04:29:17 +0000

    Added sample gyro code

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import os, time, brickpi3
 BP = brickpi3.BrickPi3()
 
 # Initialize EV3 touch sensor and motors
-BP.set_sensor_type(BP.PORT_1, BP.SENSOR_TYPE.TOUCH)
-touchButton = 0
+BP.set_sensor_type(BP.PORT_1, BP.SENSOR_TYPE.EV3_GYRO_ABS_DPS)
+gyro = 0
 
 # Without a delay reading sensor data can cause an error
 time.sleep(0.5)
@@ -17,23 +17,21 @@ try:
     while True:
 
         # Get touch sensor status
-        touchButton = BP.get_sensor(BP.PORT_1)
+        gyro = BP.get_sensor(BP.PORT_1)
             
-        # If the touch sensor is pressed
-        if touchButton:
+        BP.set_motor_power(BP.PORT_A, round(gyro[1]/5))
 
-            BP.set_motor_power(BP.PORT_A, 100)
+        # If sensor is detecting rotation
+        if gyro[1] > 0:
 
             # Turn off BrickPi on board LED
             BP.set_led(1) 
 
             # Turn on Raspberry Pi on board LED
             os.system('echo 1 | sudo dd status=none of=/sys/class/leds/led0/brightness') 
-        
-        # If the touch sensor is released
+            
+        # If no rotation is sensed
         else:
-
-            BP.set_motor_power(BP.PORT_A, 0)
 
             # Turn off BrickPi on board LED
             BP.set_led(0)
